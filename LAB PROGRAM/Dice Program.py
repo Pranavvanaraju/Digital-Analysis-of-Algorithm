420927780f667e79b5a06474f2ad7e5b62bcd5cd
V Pranav <154448052+Pranavvanaraju@users.noreply.github.com>
2024-06-17 13:15:30 +0530

    Create Dice Program.py

---
+++ LAB PROGRAM/Dice Program.py
@@ -0,0 +1,19 @@
+def printcombo(c,n):
+    for i in range(n):
+        print(c[i],end="")
+    print("")
+def generate(d,n,curr,tar):
+    if curr==n:
+        sum=0
+        for i in range(n):
+            sum+=d[i]
+        if sum==tar:
+            printcombo(d,n)
+        return
+    for i in range(1,6+1):
+        d[curr]=i
+        generate(d,n,curr+1,tar)
+n=3 
+tar=10 
+dice={}
+generate(dice,n,0,tar)
